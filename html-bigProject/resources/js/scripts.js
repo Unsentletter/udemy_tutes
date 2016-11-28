c3211b65f4c30c6b8a4bd92905cd1b03be3b365f
Tristan <tristan.southwell@hotmail.com>
2016-11-28 16:28:57 +1100

    html css course complted

--- html-bigProject/resources/js/scripts.js
+++ html-bigProject/resources/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-// FOR STICKY NAVIGATION BAR
+  // FOR STICKY NAVIGATION BAR
   $('.js--section-features').waypoint(function(direction) {
     if (direction == "down") {
       $('nav').addClass('sticky');
@@ -31,36 +31,51 @@ $(document).ready(function() {
           scrollTop: target.offset().top
         }, 1000);
         return false;
+        }
       }
+    });
+  });
+
+  // ANIMATIONS ON SCROLL
+
+  $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeIn');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated fadeInUp');
+  }, {
+    offset: '50%'
+  });
+
+  $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated pulse');
+  }, {
+    offset: '50%'
+  });
+
+  // MOBILE NAV
+  $('.js--nav-icon').click(function() {
+    var nav = $('.js--main-nav');
+    var icon = $('.js--nav-icon i');
+
+    nav.slideToggle(200);
+    if (icon.hasClass('ion-navicon-round')) {
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round')
+    } else {
+      icon.addClass('ion-navicon-round');
+      icon.removeClass('ion-close-round')
     }
   });
-});
-
-// ANIMATIONS ON SCROLL
-
-$('.js--wp-1').waypoint(function(direction) {
-  $('.js--wp-1').addClass('animated fadeIn');
-}, {
-  offset: '50%'
-});
-
-$('.js--wp-2').waypoint(function(direction) {
-  $('.js--wp-2').addClass('animated fadeInUp');
-}, {
-  offset: '50%'
-});
-
-$('.js--wp-3').waypoint(function(direction) {
-  $('.js--wp-3').addClass('animated fadeInUp');
-}, {
-  offset: '50%'
-});
-
-$('.js--wp-4').waypoint(function(direction) {
-  $('.js--wp-4').addClass('animated pulse');
-}, {
-  offset: '50%'
-});
 
 });
 
