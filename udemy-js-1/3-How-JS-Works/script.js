0215fb6013331b3a91de8e1eee33db4914427103
Tristan <tristan.southwell@hotmail.com>
2016-12-01 16:01:00 +1100

    dice game completed

---
+++ udemy-js-1/3-How-JS-Works/script.js
@@ -0,0 +1,78 @@
+///////////////////////////////////////
+// Lecture: Hoisting
+
+
+var john = {
+  name: 'John',
+  yearOfBirth: 1990,
+  calculateAge: function() {
+    console.log(this);
+    console.log(name);
+    console.log(this.name);
+  }
+}
+
+john.calculateAge()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////
+// Lecture: Scoping
+
+
+// First scoping example
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        console.log(a + b + c);
+    }
+}
+*/
+
+
+
+// Example to show the differece between execution stack and scope chain
+
+/*
+var a = 'Hello!';
+first();
+
+function first() {
+    var b = 'Hi!';
+    second();
+
+    function second() {
+        var c = 'Hey!';
+        third()
+    }
+}
+
+function third() {
+    var d = 'John';
+    console.log(a + b + c + d);
+}
+*/
+
+
+
+///////////////////////////////////////
+// Lecture: The this keyword
