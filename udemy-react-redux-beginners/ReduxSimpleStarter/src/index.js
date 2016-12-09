383c36a5353f6e000f531f96057b1025e6bd42a7
Tristan <tristan.southwell@hotmail.com>
2016-12-10 07:59:16 +1100

    update

--- udemy-react-redux-beginners/ReduxSimpleStarter/src/index.js
+++ udemy-react-redux-beginners/ReduxSimpleStarter/src/index.js
@@ -4,6 +4,7 @@ import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCwHBIRfE9zcvY8hwsvJMsI9pSdBGD4lXg';
 
@@ -14,10 +15,16 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-      this.setState({ videos });
+      this.setState({
+        videos,
+        selectedVideo: videos[0]
+      });
     });
   }
 
@@ -25,7 +32,10 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+        onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+        videos={this.state.videos} />
       </div>
     );
   }
